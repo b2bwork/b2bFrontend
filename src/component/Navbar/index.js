3ba2ac1db103681969cc3928739eb7b635c19761
jarupong pajakgo <lagmanmc@gmail.com>
2017-04-29 17:47:59 +0700

    [Template only] Navbar

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -7,25 +7,19 @@ export default class NavbarComponent extends Component{
             <Navbar collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
+        <a href="#">b2b-logo</a>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
       <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
+        <NavItem eventKey={1} href="#">ลงงาน</NavItem>
+        <NavDropdown eventKey={2} title="แจ้งเตือน" id="basic-nav-dropdown">
+          <MenuItem eventKey={2.1}>test1</MenuItem>
+          <MenuItem eventKey={2.2}>test2</MenuItem>
+        </NavDropdown>
+        <NavItem eventKey={3} href="#">เข้าสู่ระบบ</NavItem>
+        <NavItem eventKey={4} href="#">สมัครสมาชิก</NavItem>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
