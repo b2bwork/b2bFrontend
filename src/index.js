4c01840932d72fb689e0f095d6fe784d56cdefd0
jarupong pajakgo <lagmanmc@gmail.com>
2017-04-29 17:51:23 +0700

    [React] add router

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import IndexPage from './component/IndexPage/index';
+import IndexPageComponent from './component/IndexPage/index';
 import NavbarComponent from './component/Navbar/index';
 import CategoryWorksComponent from './component/CategoryWorks/index';
 import {  BrowserRouter as Router,  Route,  Link} from 'react-router-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-const networkInterface = createNetworkInterface({  uri: 'http://localhost:3000/graphql' });
+const networkInterface = createNetworkInterface({  uri: 'http://localhost:3001/graphql' });
 const client = new ApolloClient({  networkInterface});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-      <div>
         <Router>
-          <Route exact path="/" component={NavbarComponent}/>
+          <Route exact path="/" component={IndexPageComponent}/>
         </Router>
-         </div>
     </ApolloProvider>,
   document.getElementById('root')
 );
