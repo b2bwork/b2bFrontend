d5dc11c58d3a82187df9252f57194a28335ce5b0
jarupong pajakgo <lagmanmc@gmail.com>
2017-04-29 17:49:30 +0700

    [React] index page

--- src/component/IndexPage/index.js
+++ src/component/IndexPage/index.js
@@ -1,14 +1,18 @@
 import React,{Component} from 'react';
 import NavbarComponent from '../Navbar/index';
+import CategoryWorksComponent from '../CategoryWorks/index';
 
-export default class IndexPage extends Component{
+export default class IndexPageComponent extends Component{
 
     constructor(props){
         super(props);
     }
     render(){
         return (
+            <div>
             <NavbarComponent/>
+            <CategoryWorksComponent/>
+            </div>
         )
     }
 }
