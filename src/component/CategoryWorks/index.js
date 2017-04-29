be41c358e7001dd3abc7c76378254243a2a07de5
jarupong pajakgo <lagmanmc@gmail.com>
2017-04-29 17:48:42 +0700

    [React] Category Works

--- src/component/CategoryWorks/index.js
+++ src/component/CategoryWorks/index.js
@@ -1,10 +1,45 @@
 import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+import {Grid,Row,Col,Thumbnail} from 'react-bootstrap';
 
-export default class CategoryWorksComponent extends Component{
-
+const CategoryWorksQuery = gql`
+query {
+     listCategory{
+         Name
+         Image
+     }
+}
+`;
+class CategoryWorksComponent extends Component{
+   
     render(){
+        if (this.props.data.loading) {
+          return (<div></div>)
+         }
+
+        if (this.props.data.error) {
+          return (<div>เกิดปัญหาในการโหลดข้อมูลโปรดลองใหม่ภายหลัง</div>)
+         }
         return(
-            <h1>ssssss</h1>
+            <Grid>
+             <Row>
+               {this.props.data.listCategory.map((data,key) =>{
+                  return( 
+                    <div key={key}>
+                    <Link to={{ pathname: '/UnitCategory/'+ data.Name.toString() }}>
+                    <Col xs={6} md={4}>
+                    <Thumbnail alt="171x180" src={data.Image.toString()} />
+                   </Col>
+                   </Link>
+                   </div>
+                   )
+               })}
+            </Row>
+           </Grid>
         )
     }
 }
+const CategoryWorks = graphql(CategoryWorksQuery)(CategoryWorksComponent)
+export default CategoryWorks
