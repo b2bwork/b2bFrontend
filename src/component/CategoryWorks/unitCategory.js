bc142f299980c539d63c4afb2feff1d8b3101136
jarupong pajakgo <lagmanmc@gmail.com>
2017-04-30 21:13:06 +0700

    [React] list unit Category

--- src/component/CategoryWorks/unitCategory.js
+++ src/component/CategoryWorks/unitCategory.js
@@ -2,22 +2,25 @@ import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
+import { withRouter } from 'react-router-dom';
 import {Grid,Row,Col,Thumbnail} from 'react-bootstrap';
 
 const UnitCategoryWorksQuery = gql`
-    query {
-        listUnitCategory(CategoryName: "sa"){
-             Name
-             Image
-             }
+    query list($Name: String!){
+              listUnitCategory(CategoryName: $Name){
+                   Name
+                   Image
+                 }
           }
 `;
 class UnitCategoryWorksComponent extends Component{
     constructor(props){
         super(props);
     }
+    
    
     render(){
+      
         if (this.props.data.loading) {
           return (<div></div>)
          }
@@ -28,10 +31,10 @@ class UnitCategoryWorksComponent extends Component{
         return(
             <Grid>
              <Row>
-               {this.props.data.listCategory.map((data,key) =>{
+               {this.props.data.listUnitCategory.map((data,key) =>{
                   return( 
                     <div key={key}>
-                      <Link to={{ pathname: '/UnitCategory/'+ data.Name.toString() }}>
+                      <Link to={{ pathname: '/Works/'+ data.Name.toString() }}>
                        <Col xs={6} md={4}>
                         <Thumbnail alt="171x180" src={data.Image.toString()} />
                        </Col>
@@ -44,5 +47,11 @@ class UnitCategoryWorksComponent extends Component{
         )
     }
 }
-const UnitCategoryWorks = graphql(UnitCategoryWorksQuery)(UnitCategoryWorksComponent)
+const UnitCategoryWorks = graphql(UnitCategoryWorksQuery,{
+  options: (CategoryName) => ({
+      variables: {
+        Name: CategoryName.match.params.CategoryName
+      }
+    })
+  })(UnitCategoryWorksComponent)
 export default UnitCategoryWorks
