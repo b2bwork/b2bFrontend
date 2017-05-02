96792d13313394369698473fe2126f3c3bf29470
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-02 19:55:06 +0700

    [React,CSS] add carousel image

--- src/component/DetailWork/index.js
+++ src/component/DetailWork/index.js
@@ -2,7 +2,8 @@ import React,{Component} from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import {Link} from 'react-router-dom';
-import {Grid,Row,Col} from 'react-bootstrap';
+import {Grid,Row,Col,Carousel} from 'react-bootstrap';
+import './index.css';
 
 const DetailWorkQuery = gql`
       query GetDetailWork($WorkId: String){
@@ -11,6 +12,15 @@ const DetailWorkQuery = gql`
               WorkName
               CoverImage
               WorkerName
+              WorkerId
+              ScopeWork
+              Workdays 
+              DetailWork
+              ExperienceWorker
+              Price
+              Queue 
+              Image
+              TagWork
               
           }
       }
@@ -19,15 +29,33 @@ class DetailWorkComponent extends Component {
     constructor(props){
         super(props);
     }
+
     render(){
+        if (this.props.data.loading) {
+          return (<div></div>)
+         }
+
+        if (this.props.data.error) {
+          return (<div>เกิดปัญหาในการโหลดข้อมูลโปรดลองใหม่ภายหลัง</div>)
+         }
+         
         return(
+           <div className="DetailWork">
             <Grid>
-             <Row>
-               <Col xs={12} md={6}>
-               <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
+             <Row className="show-grid">
+               <Col xs={12} md={6} bsClass="btn-detail">
+               <div className="detail col-md-6 col-xs-12">
+                <Carousel>
+                 <Carousel.Item>
+                  <img width={900} height={300}src="/assets/carousel.png" className="carouselImage"/>
+                 </Carousel.Item>
+                </Carousel>
+               <h3 className="text-center">{this.props.data.DetailWork.WorkName}หหห</h3>
+               </div>
                </Col>
              </Row>
             </Grid>
+          </div>
         )
     }
 }
