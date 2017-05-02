03c705eccd10c3127b0c8b1a14b367bafc083e19
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-02 18:44:44 +0700

    [React] detail work component (imcomplete)

--- src/component/DetailWork/index.js
+++ src/component/DetailWork/index.js
@@ -3,3 +3,39 @@ import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import {Link} from 'react-router-dom';
 import {Grid,Row,Col} from 'react-bootstrap';
+
+const DetailWorkQuery = gql`
+      query GetDetailWork($WorkId: String){
+          DetailWork(_id: $WorkId){
+              CategoryName
+              WorkName
+              CoverImage
+              WorkerName
+              
+          }
+      }
+`;
+class DetailWorkComponent extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <Grid>
+             <Row>
+               <Col xs={12} md={6}>
+               <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
+               </Col>
+             </Row>
+            </Grid>
+        )
+    }
+}
+const DetailWork = graphql(DetailWorkQuery,{
+  options: (WorkId) => ({
+      variables: {
+        WorkId: WorkId.match.params.WorkId
+      }
+    })
+  })(DetailWorkComponent)
+export default DetailWork
