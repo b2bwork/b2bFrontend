103b74bcbdc5e5b21e21367249d6a42572219c0a
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-02 18:16:06 +0700

    [React] change link for show detail work

---
+++ src/component/DetailWork/index.js
@@ -0,0 +1,5 @@
+import React,{Component} from 'react';
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+import {Link} from 'react-router-dom';
+import {Grid,Row,Col} from 'react-bootstrap';
