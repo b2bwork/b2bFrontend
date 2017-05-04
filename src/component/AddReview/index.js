f7f18c2e98a6ae526f63cdcff929c6701efb3124
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-04 10:14:34 +0700

    [React,Template only] add dropzone for drop upload image

--- src/component/AddReview/index.js
+++ src/component/AddReview/index.js
@@ -4,6 +4,10 @@ import gql from 'graphql-tag';
 import {Link} from 'react-router-dom';
 import ReactStars from 'react-stars';
 import Dropzone from 'react-dropzone';
+import DropzoneComponent from 'react-dropzone-component';
+import './index.css';
+import '../../../node_modules/react-dropzone-component/styles/filepicker.css'
+import '../../../node_modules/dropzone/dist/min/dropzone.min.css'
 import {Button,Modal,FormGroup,ControlLabel,FormControl,Form} from 'react-bootstrap';
 
 class AddReviewComponent extends Component{
@@ -40,6 +44,9 @@ class AddReviewComponent extends Component{
     });
   }
     render(){
+      let componentConfig = { postUrl: 'no-url' };
+let djsConfig = { autoProcessQueue: false }
+let eventHandlers = { addedfile: (file) => console.log(file) }
       return(
           <div>
           <Button
@@ -55,18 +62,14 @@ class AddReviewComponent extends Component{
           </Modal.Header>
           <Modal.Body>
               <ReactStars count={5} size={24}  color2={'#ffd700'} />
-           <Form inline>
-            <FormGroup controlId="formControlsTextarea">
-             <ControlLabel>รายละเอียด</ControlLabel>
-             <FormControl componentClass="textarea" placeholder="textarea" />
-             </FormGroup>
-             <FormGroup controlId="formControlsTextarea">
-             <ControlLabel>รูปภาพ</ControlLabel>
-             <Dropzone onDrop={this.onDrop.bind(this)}>
-              <p>อัพโหลดรูปภาพงาน</p>
-             </Dropzone>
-             </FormGroup>
-           </Form>
+          <div class="form-group">
+           <label class="col-sm-2 control-label">รายละเอียด</label>
+           <div class="col-sm-10">
+             <textarea onChange={(change) => this.setState({Reviewdata: change.target.value})}></textarea>
+           </div>
+           </div>
+           <label class="col-sm-2 control-label">รูปภาพงาน</label>
+              <DropzoneComponent config={componentConfig} eventHandlers={eventHandlers} djsConfig={djsConfig} />
           </Modal.Body>
         </Modal>
         </div>
