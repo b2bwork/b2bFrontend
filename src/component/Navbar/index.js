866e48d4d9a6eaf93992d020ebaeb53281a0abd5
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-18 22:59:55 +0700

    [React,Bulma] new navbar

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -1,29 +1,37 @@
 import React,{Component} from 'react';
-import {Navbar,Nav,NavItem,NavDropdown,MenuItem} from 'react-bootstrap';
+import '../../Bulmacss/bulma.css';
 import './index.css';
 
 export default class NavbarComponent extends Component{
     render(){
         return (
-            <Navbar collapseOnSelect className="btn-navbar">
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">b2b-logo</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">ลงงาน</NavItem>
-        <NavDropdown eventKey={2} title="แจ้งเตือน" id="basic-nav-dropdown">
-          <MenuItem eventKey={2.1}>test1</MenuItem>
-          <MenuItem eventKey={2.2}>test2</MenuItem>
-        </NavDropdown>
-        <NavItem eventKey={3} href="#">เข้าสู่ระบบ</NavItem>
-        <NavItem eventKey={4} href="#">สมัครสมาชิก</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+            <nav className="nav bg">
+              <div className="container">
+                <div className="nav-left">
+                  <a className="nav-item">
+                    B2B Product
+                  </a>
+                </div>
+           <label htmlFor="menu-toggle" className="nav-toggle">
+             <span></span>
+             <span></span>
+             <span></span>
+           </label>
+          <input type="checkbox" id="menu-toggle" className="is-hidden"/>
+
+          <div className="nav-right nav-menu">
+            <a className="nav-item">
+              เข้าสู่ระบบ / สมัคร
+            </a>
+            <a className="nav-item">
+              เพิ่มงาน
+            </a>
+            <a className="nav-item">
+              แจ้งเตือน<i className="fa fa-bell" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </nav>
         )
     }
 }
