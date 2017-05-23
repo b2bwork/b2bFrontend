3560d2448e6a32fe1e20f31532c204e6008e10da
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-23 22:14:12 +0700

    [Router] add registerd router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route ,Switch , Redirect} from 'react-router-dom';
 import './index.css';
+import { LocaleProvider } from 'antd';
+import enUS from '../node_modules/antd/lib/locale-provider/en_US';
+
+
 import IndexPageComponent from './component/IndexPage/index';
 import listWorksComponent from './component/ListWorks/index';
 import UnitCategoryWorksComponent from './component/CategoryWorks/unitCategory';
 import DetailWork from './component/DetailWork/index';
 import Register from './component/AuthenModal/register';
-import { BrowserRouter as Router, Route ,Switch , Redirect} from 'react-router-dom';
+import registeredComponent from './component/AuthenModal/registerd';
+
+
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { LocaleProvider } from 'antd';
-import enUS from '../node_modules/antd/lib/locale-provider/en_US';
 import { ApolloProvider } from 'react-apollo';
 const networkInterface = createNetworkInterface({  uri: 'http://localhost:3001/graphql' });
 const client = new ApolloClient({  networkInterface});
@@ -26,6 +31,7 @@ ReactDOM.render(
           <Route exact={true} path="/Register" component={Register}/>
           <Route exact={true} path="/logout" render={() => (<Redirect to="/"/>)}/>
           <Route exact={true} path="/loged" render={() => (<Redirect to="/"/>)}/>
+          <Route exact={true} path="/registered" component={registeredComponent}/>
           </Switch>
         </Router>
        </LocaleProvider>
