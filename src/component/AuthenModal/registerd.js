b7989816c30f7914912a22ff2d4624b73472b741
jarupong pajakgo <lagmanmc@gmail.com>
2017-06-07 23:44:17 +0700

    Remove module don't use

--- src/component/AuthenModal/registerd.js
+++ src/component/AuthenModal/registerd.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import {graphql} from 'react-apollo';
-import gql from 'graphql-tag';
 
-import '../../../node_modules/antd/dist/antd.min.css';
-import {Row , Col} from 'antd';
 import NavbarComponent from '../Navbar/index';
 
 export default class registeredComponent extends Component {
