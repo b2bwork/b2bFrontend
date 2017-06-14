d66c9a2456d802e3422861c63bca6f0783152018
jarupong pajakgo <lagmanmc@gmail.com>
2017-06-14 21:45:24 +0700

    [DropzoneJS] add component for user upload ID card image

--- src/component/userProfile/IdCard.js
+++ src/component/userProfile/IdCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import { Form , Col , Row , Button , Select } from 'antd';
+import DropzoneComponent from 'react-dropzone-component';
 
 import '../../../node_modules/antd/dist/antd.min.css';
 import './index.css';
