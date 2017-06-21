16c9391a648c1b3206a7fc8d0fb73bff446c4af4
jarupong pajakgo <lagmanmc@gmail.com>
2017-06-21 22:35:44 +0700

    [ContentLoader] add loding component before load data

--- src/component/listFreelanceWork/index.js
+++ src/component/listFreelanceWork/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
+import {Link} from 'react-router-dom';
 
 import { Button , Input , Col , Row , Icon , Modal , InputNumber} from 'antd';
 import NavbarComponent from '../Navbar/index';
@@ -21,10 +22,40 @@ const listFreelanceWorkQuery = gql`
 `
 class listFreelanceWorkComponent extends Component {
     render() {
-        return (
-            <div>
-                <NavbarComponent/>
-            </div>
+        if (this.props.data.loading) {
+          return (
+              <div>
+                  <NavbarComponent/>
+                  <ContentLoader height={140} speed={1} primaryColor={'#f3f3f3'} secondaryColor={'#ecebeb'}>
+                    <Rect x={50} y={80} height={10} radius={5} width={300} />
+                  </ContentLoader>
+              </div>
+          )
+         }
+
+        if (this.props.data.error) {
+          return (<div>เกิดปัญหาในการโหลดข้อมูลโปรดลองใหม่ภายหลัง</div>)
+         }
+
+    return (
+        <div>
+            <NavbarComponent/>
+            {this.props.data.listFreelanceWorks.map((data,key)=>{
+               return( 
+                    <div key={key}>
+                      <Link to={{ pathname: '/editWork/'+ data._id }}>
+                       <Col md={4} offset={1}>
+                        <img src={data.CoverImage} width="100%" alt=""/>
+                        <h3>{data.WorkName}</h3>
+                        <p><span><Icon type="shopping"/>{"  "+data.Queue}</span></p>
+                        <p><span><Icon type="shopping"/>{"  "+data.Verify}</span></p>
+                       </Col>
+                      </Link>
+                   </div>
+                   )
+            })
+            }
+        </div>
         );
     }
 }
